8d2a95e5469bbab8ff2706e6590ac525800da753
xiolng <zheng_zhanning@126.com>
2019-02-15 16:58:15 +0800

    build

---
+++ build/precache-manifest.cfc4e3ccbe953c3aa6cc514647fb1f65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "969d901d27f9c683f53c",
+    "url": "/static/js/main.969d901d.chunk.js"
+  },
+  {
+    "revision": "10ba2787a36d5f48c206",
+    "url": "/static/js/1.10ba2787.chunk.js"
+  },
+  {
+    "revision": "969d901d27f9c683f53c",
+    "url": "/static/css/main.c9102e22.chunk.css"
+  },
+  {
+    "revision": "10ba2787a36d5f48c206",
+    "url": "/static/css/1.bb648af0.chunk.css"
+  },
+  {
+    "revision": "43bb54b6b75ebd17bae236916b13914e",
+    "url": "/index.html"
+  }
+];
